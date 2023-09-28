2f1db71723a8a07318866a8452632bc06f25b679
Vishal101022 <raivishal939@gmail.com>
2023-09-28 13:11:06 +0530

    customised tailwind classes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,17 +1,16 @@
-
-import  Sidebar  from "./_components/sidebar";
 import Navbar from "./_components/navbar";
+import Sidebar from "./_components/sidebar";
 
-const DashboardLayout = ({ childern }: { childern: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className="h-[70px] md:pl-56 fixed inset-y-0 w-full z-50">
+      <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
         <Navbar />
       </div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
-      <main className="md:pl-56 h-full">{childern}</main>
+      <main className="md:pl-56 pt-[80px] h-full">{children}</main>
     </div>
   );
 };
