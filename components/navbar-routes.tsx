5c97c75375234d1cdfe66f7ba3b2f8e557abed74
Vishal101022 <raivishal939@gmail.com>
2023-09-28 12:57:57 +0530

    add dynamic routes

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,12 +1,37 @@
-"use client"
-import { UserButton } from "@clerk/nextjs"
+"use client";
+import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 const NavbarRoutes = () => {
+  const pathname = usePathname();
+
+  // dynamic constant
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  // for individual course page
+  const isPlayerPage = pathname?.includes("/chapter");
+
   return (
     <div className="flex gap-x-2 ml-auto">
-        <UserButton />
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button size="sm" variant="outline">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="outline">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
-  )
-}
+  );
+};
 
-export default NavbarRoutes
+export default NavbarRoutes;
