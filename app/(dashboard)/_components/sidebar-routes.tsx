9b0120e81e58cff7275ab4d4e6ac5c3a3c8b7e66
Vishal101022 <raivishal939@gmail.com>
2023-09-28 12:55:01 +0530

    add teacherRoutes

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,31 +1,50 @@
-"use client"
-import { Layout, Compass } from 'lucide-react'
-import { SidebarItem } from './sidebar-item'
+"use client";
+import { usePathname } from "next/navigation";
+import { Layout, Compass, BarChart, List } from "lucide-react";
+import { SidebarItem } from "./sidebar-item";
+
+// when you click on the exit button, you will be redirected to the home page
 const guestRoutes = [
-    {
-        icon: Layout,
-        label: 'Dashboard',
-        href: '/'
-    },
-    {
-        icon: Compass,
-        label: 'Browse',
-        href: '/search'
-    },
-]
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Browse",
+    href: "/search",
+  },
+];
+// when you click on the teacher mode button, you will be redirected to the teacher mode page
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
 
 export const SidebarRoutes = () => {
-    const routes = guestRoutes;
-    return(
-        <div className="flex flex-col w-full">
-           {routes.map((route) => (
-            <SidebarItem 
-                key={route.label}
-                icon={route.icon}
-                label={route.label}
-                href={route.href}
-            />
-           ))}
-        </div>
-    )
-}
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.includes("/teacher");
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
+
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.label}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
