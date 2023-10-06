4f7b67ed1718462d7a583ccfe118621514ec8312
Vishal101022 <raivishal939@gmail.com>
2023-10-06 11:01:31 +0530

    :rotating_light: remove unused imports

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -2,12 +2,11 @@
 
 import * as z from "zod";
 import axios from "axios";
-import { Pencil, PlusCircle, ImageIcon, File, Loader2, X } from "lucide-react";
+import { PlusCircle, File, Loader2, X } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { Attachment, Course } from "@prisma/client";
-import Image from "next/image";
 
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
