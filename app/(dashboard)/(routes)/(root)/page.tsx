58cf231ad9d62e86dfce3d8c43199d3320c73b98
Vishal101022 <raivishal939@gmail.com>
2023-10-16 20:14:08 +0530

    student dashboard home

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,9 +1,39 @@
-import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { CheckCircle, Clock } from "lucide-react";
 
-export default function Home() {
-  return(
-    <div className="flex justify-end">
-    <UserButton afterSignOutUrl="/"/>
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+import { CoursesList } from "@/components/course-list";
+
+import { InfoCard } from "./_components/info-card";
+
+export default async function Dashboard() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { completedCourses, coursesInProgress } = await getDashboardCourses(
+    userId
+  );
+
+  return (
+    <div className="p-6 space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <InfoCard
+          icon={Clock}
+          label="In Progress"
+          numberOfItems={coursesInProgress.length}
+        />
+        <InfoCard
+          icon={CheckCircle}
+          label="Completed"
+          numberOfItems={completedCourses.length}
+          variant="success"
+        />
+      </div>
+      <CoursesList items={[...coursesInProgress, ...completedCourses]} />
     </div>
-  )
+  );
 }
