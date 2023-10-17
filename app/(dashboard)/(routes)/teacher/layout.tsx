aa2ca7234ace1ccddebed8f86889fa5a70b6e495
Vishal101022 <raivishal939@gmail.com>
2023-10-17 10:37:00 +0530

    :bug: fix bug

--- app/(dashboard)/(routes)/teacher/layout.tsx
+++ app/(dashboard)/(routes)/teacher/layout.tsx
@@ -1,17 +1,14 @@
-
-import { isTeacher } from "@/lib/teacher"
-import { auth } from "@clerk/nextjs"
-import { redirect } from "next/navigation"
+import { isTeacher } from "@/lib/teacher";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 const TeacherLayout = ({ children }: { children: React.ReactNode }) => {
-    const { userId } = auth()
+  const { userId } = auth();
 
-    if(!isTeacher(userId)) {
-        return redirect("/")
-    }
-  return (
-    {children}
-  )
-}
+  if (!isTeacher(userId)) {
+    return redirect("/");
+  }
+  return <>{children}</>;
+};
 
-export default TeacherLayout
+export default TeacherLayout;
