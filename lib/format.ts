98bb74e83bb4dd17db462bc8f359c48d8453fa9c
Vishal101022 <raivishal939@gmail.com>
2023-10-18 06:54:15 +0530

    change to Indian currency

--- lib/format.ts
+++ lib/format.ts
@@ -1,6 +1,6 @@
 export const formatPrice = (price: number) => {
-    return new Intl.NumberFormat("en-US", {
+    return new Intl.NumberFormat("en-IN", {
       style: "currency",
-      currency: "USD"
+      currency: "INR"
     }).format(price)
   }
